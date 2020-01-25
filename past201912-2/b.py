0af1fb20513749ed3c89482e54a423ec983b147f
Shigeo Kitamura <shigeo9462@gmail.com>
2020-01-25 19:11:22 +0900

    past201912-2_b

---
+++ past201912-2/b.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+A = [int(input()) for i in range(N)]
+
+for i in range(1, N):
+    diff = A[i] - A[i-1]
+
+    if diff == 0:
+        print("stay")
+    elif diff > 0:
+        print("up %s" % diff)
+    elif diff < 0:
+        print("down %s" % (diff * (-1)))
